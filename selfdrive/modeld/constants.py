65bac0120253aa507cbfadcc9e0a5a308cf70fb5
KTH-8805 <nubab999@gmail.com>
2026-04-03 16:46:51 +0900

    Update constants.py

--- selfdrive/modeld/constants.py
+++ selfdrive/modeld/constants.py
@@ -1,7 +1,7 @@
 IDX_N = 33
 
 def index_function(idx, max_val=192, max_idx=32):
-  return (max_val) * ((idx/max_idx)**2)
+  return (max_val) * ((idx/max_idx)**2.0)
 
 
-T_IDXS = [index_function(idx, max_val=10.0) for idx in range(IDX_N)]
+T_IDXS = [index_function(idx, max_val=15.0) for idx in range(IDX_N)]
